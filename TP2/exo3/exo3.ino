a0bbf1930651c0342a7d74c4cd123811fecde9cb
KieceDonc <valentinverst.developer@gmail.com>
2020-11-24 17:03:07 +0100

    update TP2

--- TP2/exo3/exo3.ino
+++ TP2/exo3/exo3.ino
@@ -1,4 +1,3 @@
-
 int analogOutput = A0;
 
 void setup() {
@@ -7,12 +6,15 @@ void setup() {
 }
 
 void loop() {
-  for(int x=0;x<1024;x+=10){ // incrémente le voltage d'environ 0.05V toutes les 20 ms en partant de 0
+  // incrémente le voltage d'environ 0.05V toutes les 20 ms en partant de 0
+  for(int x=0;x<1024;x+=10){ 
     analogWrite(analogOutput,x);
     Serial.println(x);
     delay(20);
   }
-  for(int x=1023;x>=0;x-=10){// décrémente le voltage d'environ 0.05V toutes les 20 ms en partant de 5V
+  
+  // décrémente le voltage d'environ 0.05V toutes les 20 ms en partant de 5V
+  for(int x=1023;x>=0;x-=10){
     analogWrite(analogOutput,x);
     Serial.println(x);
     delay(20);
